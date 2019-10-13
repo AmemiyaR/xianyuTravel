a0510718547e4c7125a0e67c179dc0b084a8ddd2
AmemiyaR <53567701+AmemiyaR@users.noreply.github.com>
2019-10-13 21:05:09 +0800

    创建注册的组件

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -24,7 +24,7 @@
                 <LoginForm v-if="currentTab == 0"/>
 
                 <!-- 注册功能组件 -->
-                <!-- <RegisterForm v-if="currentTab == 1"/> -->
+                <RegisterForm v-if="currentTab == 1"/>
             </div>
         </el-row>
     </div>
@@ -32,6 +32,7 @@
 
 <script>
 import LoginForm from '@/components/user/loginForm'
+import RegisterForm from '@/components/user/registerForm'
 export default {
     data(){
         return{
@@ -39,7 +40,8 @@ export default {
         }
     },
     components:{
-        LoginForm
+        LoginForm,
+        RegisterForm
     },
     methods:{
         handleChangeTab(index){
