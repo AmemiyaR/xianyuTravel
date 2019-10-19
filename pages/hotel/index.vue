29889d971f6bc81b35a0b8677be5372eff1099ef
AmemiyaR <53567701+AmemiyaR@users.noreply.github.com>
2019-10-19 19:03:23 +0800

    组件销毁时候终止定时器
    从付款页面切换页面时,为了使在其他页面不调用付款页面的定时器请求付款状况,我们在付款页面中使用跳转页面时调用的destory清除定时器

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,13 +1,28 @@
 <template>
-    <div></div>
+    <div>
+        <script type="text/javascript" src="https://webapi.amap.com/maps?v=1.4.15&key=798680e2d8f932660dedaa9796623ed4"></script> 
+    <div id="container"></div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    mounted(){
+        window.onLoad = function(){ 
+                var map = new AMap.Map('container'); 
+            } 
+            var url = 'https://webapi.amap.com/maps?v=1.4.15&key=798680e2d8f932660dedaa9796623ed4&callback=onLoad'; 
+            var jsapi = document.createElement('script'); 
+            jsapi.charset = 'utf-8'; 
+            jsapi.src = url; 
+            document.head.appendChild(jsapi);
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+#container {
+    width: 600px;
+    height: 600px;
+}
 </style>
